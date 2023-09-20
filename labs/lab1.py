abee0f64c00c4cf4436bda83873b2fd4825c2657
kristinanazukina <144202146+kristinanazukina@users.noreply.github.com>
2023-09-20 11:00:05 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,4 +1,7 @@
 import math
-
-def tack1():
-  pass
+x=input(float("введи знач:"))
+y=input(float("введи знач:"))
+z=input(float("введи знач:"))
+a=(sqrt(abs(x**3/2)) - cos(y))/(1+2*x+ln(y))
+b=sqrt(x**2/3)-cos(y)+z+ln(y)
+print(a,b)
