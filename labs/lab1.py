fcd3f8102455bf5c940f479585e128e50ea56fcf
xyz <kristinanazukina7@gmail.com>
2023-09-26 23:17:00 +0500

    2 задание

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,9 +1,9 @@
 from math import *
-x = float(input("Введите значение переменной x "))
-y = float(input("Введите значение переменной y "))
-z = float(input("Введите значение переменной z "))
-a=(sqrt(abs(x**3/2))-cos(y))/(1+2*x+log(y))
-b=sqrt(x**2/3)-cos(y)+ z+ log(y)
-print("Значение a = ", a, "Значение b = ", b)
+x=float(input("введите значение переменной="))
+a=2
+b=-1
+f=((sqrt(x))/(sqrt(x+a))) + x**b
+print('f=',f)
+
 
 
